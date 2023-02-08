775ca92946158c9b9e9d74539a418a98696b7084
gshah2020 <gaurang.r.shah@gmail.com>
2023-02-08 15:05:36 -0500

    remove demo collection

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -6,28 +6,6 @@ import { routers } from './routers';
 export const schema = defineSchema({
   collections: [
     {
-      name: 'post',
-      label: 'Posts',
-      path: '_content/posts',
-      format: 'mdx',
-      fields: [
-        {
-          type: 'string',
-          name: 'title',
-          label: 'Title',
-          isTitle: true,
-          required: true,
-        },
-        {
-          type: 'rich-text',
-          name: 'body',
-          label: 'Body',
-          isBody: true,
-        },
-      ],
-      ui: { router: routers.post },
-    },
-    {
       name: 'page',
       label: 'Pages',
       path: '_content/pages',
