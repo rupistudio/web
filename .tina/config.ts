3712f618e9e331affb8baaece7abeaa232dfea03
gshah2020 <gaurang.r.shah@gmail.com>
2023-02-08 14:13:01 -0500

    .tina update tina env var naming scheme

--- .tina/config.ts
+++ .tina/config.ts
@@ -5,9 +5,15 @@ import { schema } from './schema';
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main';
 
 export default defineConfig({
-  branch: process.env.NEXT_PUBLIC_TINA_BRANCH || branch,
-  clientId: String(process.env.NEXT_PUBLIC_TINA_CLIENT_ID), // Get this from tina.io
-  token: String(process.env.NEXT_PUBLIC_TINA_READ_ONLY_TOKEN), // Get this from tina.io
+  branch: process.env.TINA_PUBLIC_BRANCH || branch,
+  clientId: String(
+    // 'ea0462d5-24f5-4b80-acfd-71bb3c26cd2c' ||
+    process.env.TINA_PUBLIC_CLIENT_ID
+  ), // Get this from tina.io
+  token: String(
+    // '9d67782a084d1980113d45e9a0042107671ac94a' ||
+    process.env.TINA_PUBLIC_READ_ONLY_TOKEN
+  ), // Get this from tina.io
   build: {
     outputFolder: 'admin',
     publicFolder: 'public',
