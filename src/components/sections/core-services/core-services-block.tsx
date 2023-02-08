625d10ef489b15376cad654c7b2d15a62d8e2992
gshah2020 <gaurang.r.shah@gmail.com>
2023-02-08 18:53:10 -0500

    UI: core-services: make bg full-width

--- src/components/sections/core-services/core-services-block.tsx
+++ src/components/sections/core-services/core-services-block.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Container, SimpleGrid } from '@chakra-ui/react';
+import { AspectRatio, Box, Container, SimpleGrid } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 
 import type {
@@ -26,46 +26,50 @@ export const CoreServicesBlock: React.FC<PageSectionsCoreServices> = (
 ) => {
   const router = useRouter();
   return (
-    <Container
-      as="section"
-      id="services"
-      position="relative"
-      maxW="container.xl"
-      centerContent
-      p={{ sm: 12, md: 16 }}
-      pt={{ lg: 32 }}
-      backgroundImage="linear-gradient(180deg, #FFF1E4 0%, #FED7E2 70%, #FFF1E4 100%)" // bg > pink.100
+    <Box
+      w="full"
+      backgroundImage="linear-gradient(180deg, #FFF1E4 0%, #FED7E2 80%, #FFF1E4 100%)" // bg > pink.100
     >
-      <Title title={"Rupi's Core Services"} />
-      <SimpleGrid
-        w="full"
-        my={16}
-        p={{ base: 4, lg: 12 }}
-        gap={9}
-        alignItems="center"
-        columns={[1, 2, 3]}
-        autoRows="1fr"
-        justifyContent="center"
-        border="6px dashed"
-        borderColor="red.200"
-        borderRadius="10px"
+      <Container
+        as="section"
+        id="services"
+        position="relative"
+        maxW="container.xl"
+        centerContent
+        p={{ sm: 12, md: 16 }}
+        pt={{ lg: 32 }}
       >
-        {props.services?.length
-          ? props.services?.map(
-              // @FIXME: update type
-              (service: any) =>
-                service?.service && (
-                  <CoreServiceBox
-                    key={service?.service?.title}
-                    item={service?.service}
-                  />
-                )
-            )
-          : null}
-        <AspectRatio ratio={16 / 9}>
-          <MoreBox onClick={() => router.push('/services')} />
-        </AspectRatio>
-      </SimpleGrid>
-    </Container>
+        <Title title={"Rupi's Core Services"} />
+        <SimpleGrid
+          w="full"
+          my={16}
+          p={{ base: 4, lg: 12 }}
+          gap={9}
+          alignItems="center"
+          columns={[1, 2, 3]}
+          autoRows="1fr"
+          justifyContent="center"
+          border="6px dashed"
+          borderColor="red.200"
+          borderRadius="10px"
+        >
+          {props.services?.length
+            ? props.services?.map(
+                // @FIXME: update type
+                (service: any) =>
+                  service?.service && (
+                    <CoreServiceBox
+                      key={service?.service?.title}
+                      item={service?.service}
+                    />
+                  )
+              )
+            : null}
+          <AspectRatio ratio={16 / 9}>
+            <MoreBox onClick={() => router.push('/services')} />
+          </AspectRatio>
+        </SimpleGrid>
+      </Container>
+    </Box>
   );
 };
