303c8437019298ff8f22ada5f3fc60eb63420af0
gshah2020 <gaurang.r.shah@gmail.com>
2023-02-08 23:48:37 -0500

    .tina content email-cta defaults

--- .tina/blocks/ctas/email-cta.ts
+++ .tina/blocks/ctas/email-cta.ts
@@ -6,6 +6,14 @@ export const emailCta: Template = {
   label: 'CTA: Email Subscription',
   name: 'emailCta',
   ui: {
+    defaultItem: {
+      image: {
+        src: '/uploads/people/girl-holding-money-pink-bg.webp',
+      },
+      cta: 'Sign up for our Mailing List',
+      description:
+        'Get the latest in beauty news, updates, and special offers delivered directly to your inbox. No spam, ever.',
+    },
     previewSrc: '/blocks/preview-email-cta.webp',
   },
   fields: [
