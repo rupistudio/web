3e26fe1f8535bc5c5b73fd3030763ce9b6ece86b
gshah2020 <gaurang.r.shah@gmail.com>
2023-02-10 14:52:04 -0500

    add default props for divider component

--- src/components/sections/divider.tsx
+++ src/components/sections/divider.tsx
@@ -9,11 +9,15 @@ export const DividerBlock: React.FC<any> = ({
   orientation = 'horizontal',
   borderColor = 'gray.500',
 }) => {
+  const border = borderColor?.includes('-')
+    ? borderColor.replace('-', '.')
+    : borderColor;
+
   return (
     <Box w="full" h="full" maxW="85%" maxH="85%" mx="auto" my="auto" py={6}>
       <Divider
-        orientation={orientation}
-        borderColor={borderColor.replace('-', '.')}
+        orientation={orientation || 'horizontal'}
+        borderColor={border || 'gray.500'}
       />
     </Box>
   );
